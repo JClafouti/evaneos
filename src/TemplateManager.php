a3579abc7005306a3df82416a4dccc893581c2b5
COIN-PC\Coin <justin.pouant@gmail.com>
2017-07-23 20:17:21 +0200

    application de la methode à destinatioon link

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -42,16 +42,13 @@ class TemplateManager
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
 
         $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
+        $_user = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
 
         if ($quote)
         {
             $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
             $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
             $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
             
             // replace summary_html
             $text = $this->replaceTemplateVar($text, 'quote:summary_html', Quote::renderHtml($_quoteFromRepository));
@@ -61,20 +58,21 @@ class TemplateManager
             
             // replace detination name
             $text = $this->replaceTemplateVar($text, 'quote:destination_name', $destinationOfQuote->countryName);
+
+            // replace destination link
+            $text = $this->replaceTemplateVar($text, 'quote:destination_link', 
+                $usefulObject->url . 
+                '/' . 
+                DestinationRepository::getInstance()->getById($quote->destinationId)->countryName . 
+                '/quote/' . 
+                $_quoteFromRepository->id
+                );
             
         }
-
-        // replace destination link
-        $text = $this->replaceTemplateVar($text, 'quote:destination_link', '');
-
-        if (isset($destination))
-            $text = $this->replaceTemplateVar($text, 'quote:destination_link', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id);        
-                   
 
         /*
          * replace user first name
          */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
         if($_user) {
             $text = $this->replaceTemplateVar($text, 'user:first_name', ucfirst(mb_strtolower($_user->firstname)));
         }
